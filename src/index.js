708c7c2367743cb3912a1f6536d67e2282e39bd6
jvitorc <jvitoroc@gmail.com>
2018-06-21 20:28:09 -0300

    simplified

--- src/index.js
+++ src/index.js
@@ -18,15 +18,12 @@ function getBrowserLocale(){
 let localizer = (function () {
 
     const DEFAULT_CONFIG = {
-        endpoint: "/translations/", // endpoint: where to fetch the JSONs
+        endpoint: "/strings/",      // endpoint: where to fetch the JSONs
         ext: ".json",               // ext: extension of the resources
         default: "en",              // default: default language
-        forceDefault: false,        // forceDefault: if it's true always fetch the default language, otherwise fetch accordindly to the user's language
-        cacheLast: true,            // cacheLast: cache (overwrite the previous one) the current language's JSON in the localstoage
-        cacheDefault: false,        // cacheDefault: cache (overwrite the previous one) default language in the localstorage
         changingClass: "changing",  // changingClass: class name that will be added to the elements with the [data-localize] attribute when a translation is being loaded
         priority: 'language',       // priority(language|country): (consider pt-BR) if the priority is the 'language', then localizer will request 'pt' first, if it's not successful request 'pt-BR'. If the priority is the 'country', it will do the inverse.
-        country: true               // country: should localizer request country?
+        country: true               // country: should localizer request country (pt-BR)?
     }
 
     const storage = window.localStorage;
@@ -55,47 +52,14 @@ let localizer = (function () {
         });
     }
 
-    localizer.prototype._get = async function(resource){
-        return await this._fetcher('get', `${this.config.endpoint}${resource}${this.config.ext}`);
-    }
-
-    localizer.prototype._cacheDefault = async function(){
-        if(storage.getItem("default_lc_name") != this.config.default){
-            storage.setItem("default_json", await this._get(this.config.default));
-            storage.setItem("default_lc_name", this.config.default);
-        }
-    }
-
     localizer.prototype.init = function(){
         this._callEvent('initialstart');
         this._localize();
         this._callEvent('initialend');
     }
 
-    localizer.prototype._fetch = function(lang){
-        return new Promise((resolve, reject)=>{
-            try{
-                if(this.config.cacheLast){
-                    if(storage.getItem("lc_name") == lang){
-                        let data = storage.getItem("lang_json");
-                        if(data)
-                            resolve(JSON.parse(data));
-                    }else{  
-                        this._get(lang).then((data)=>{
-                            resolve(data);
-                            storage.setItem("lang_json", JSON.stringify(data));
-                            storage.setItem("lc_name", lang);
-                        });
-                    }
-                    return;
-                }
-                this._get(lang).then((data)=>{
-                    resolve(data);
-                });
-            }catch(e){
-                reject(e); //reject whatever
-            }
-        });
+    localizer.prototype._fetch = async function(resource){
+        return await this._fetcher('get', `${this.config.endpoint}${resource}${this.config.ext}`);
     }
 
     localizer.prototype._populate = function (data){
@@ -124,16 +88,17 @@ let localizer = (function () {
                 break;
             }catch(e){continue;}
         }
-        if(null)
+        if(data)
             throw new Error("An error occurred when trying to fetch the data");
         return data;
     }
 
-    localizer.prototype._localize = async function(resource){
+    localizer.prototype._localize = async function(){
         let data;
-        if(!this.config.forceDefault){
+        let resources = [];
+        let chacedLang = storage.getItem("localizer_lang");
+        if(!chacedLang){
             const locale = getBrowserLocale();
-            let resources = [];
             switch(this.config.priority){
                 case "country":
                     if(locale[1])
@@ -148,11 +113,11 @@ let localizer = (function () {
                         resources.push(locale[1]);
                 break;
             }
-            resources.push(this.config.default);
-            data = await this._fetchFirst(resources);
-        }else{
-            data = await this._fetch(this.config.default);
         }
+        else
+            resources.push(chacedLang);
+        resources.push(this.config.default);
+        data = await this._fetchFirst(resources);
         this._populate(data);
     }
 
@@ -164,9 +129,9 @@ let localizer = (function () {
     
     localizer.prototype.change = function(lang){
         this._callEvent('changestart', {lang});
-        this.config.currentLang = lang;
+        storage.setItem("localizer_lang", lang);
         
-        this._localize(lang);
+        this._localize();
 
         this._callEvent('changeend', {lang});
     }
